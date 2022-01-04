fdd7ddef65499c1eb5e15bb96317294ace50fb7c
jiyujia926 <84773677+jiyujia926@users.noreply.github.com>
2022-01-04 22:16:32 +0800

    前端修改

--- code/front-end/src/App.js
+++ code/front-end/src/App.js
@@ -6,8 +6,6 @@ import Login from './apps/Login';
 import Home from './apps/Home';
 import Annotate from './apps/Annotate';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Circle from './apps/Draw';
-import Drawboard from './apps/Drawboard';
 import MissionDetail from './apps/MissonDetail';
 import CheckAnnotation from './apps/CheckAnnotation';
 import AnnotationList from './apps/Annotaionlist';
@@ -20,7 +18,6 @@ class App extends React.Component{
             <Route path="/register" element={<Register/>}/>
             <Route path="/home" element={<Home/>}/>
             <Route path="/annotate/:mid" element={<Annotate/>}/>
-            <Route path="/drawboard" element={<Drawboard/>}/>
             <Route path="/mission/:mid" element={<MissionDetail />}/>
             <Route path="/check/:mid/:username" element={<CheckAnnotation />}/>
             <Route path="/annotationlist/:mid" element={<AnnotationList />}/>
